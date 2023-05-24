04b6da062931465655cd681a3e2d40b4846966fa
FabianWild <Fabian.Wild1996@gmx.de>
2023-05-24 19:59:08 +0200

    ---

--- leaflet.py
+++ leaflet.py
@@ -3,11 +3,7 @@ from dash import html
 import dash_leaflet as dl
 import datetime
 import requests
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.image import imread
-import io
-from io import BytesIO
+
 
 app = dash.Dash(__name__)
 
@@ -41,77 +37,32 @@ app.layout = html.Div([
 
 # Callback to synchronize map center and zoom
 @app.callback(
-    dash.dependencies.Output('map1', 'center'),
-    dash.dependencies.Output('map2', 'center'),
-    dash.dependencies.Output('map1', 'zoom'),
-    dash.dependencies.Output('map2', 'zoom'),
+    [dash.dependencies.Output('map1', 'center'),
+     dash.dependencies.Output('map2', 'center'),
+     dash.dependencies.Output('map1', 'zoom'),
+     dash.dependencies.Output('map2', 'zoom')],
     [dash.dependencies.Input('map1', 'center'),
      dash.dependencies.Input('map2', 'center'),
      dash.dependencies.Input('map1', 'zoom'),
      dash.dependencies.Input('map2', 'zoom')]
 )
 def update_map(map1_center, map2_center, map1_zoom, map2_zoom):
-    new_center = map1_center if map1_center is not None else map2_center
-    new_zoom = map1_zoom if map1_zoom is not None else map2_zoom
+    if map1_center and map2_center:
+        new_center = map1_center
+    elif map1_center:
+        new_center = map1_center
+    else:
+        new_center = map2_center
+        
+    if map1_zoom and map2_zoom:
+        new_zoom = map1_zoom
+    elif map1_zoom:
+        new_zoom = map1_zoom
+    else:
+        new_zoom = map2_zoom
+
     return new_center, new_center, new_zoom, new_zoom
 
-
-@app.callback(
-    dash.dependencies.Output('map1', 'bounds'),
-    dash.dependencies.Output('map2', 'bounds'),
-    [dash.dependencies.Input('map1', 'click_lat_lng'),
-     dash.dependencies.Input('map2', 'click_lat_lng')]
-)
-def update_map_bounds(map1_click, map2_click):
-    return map1_click, map2_click
-
-@app.callback(
-    dash.dependencies.Output('map1', 'children'),
-    dash.dependencies.Output('map2', 'children'),
-    [dash.dependencies.Input('map1', 'bounds'),
-     dash.dependencies.Input('map2', 'bounds')]
-)
-def update_map_layers(map1_bounds, map2_bounds):
-    date = datetime.date(2022, 1, 23)  # Replace with your desired date
-
-    url = 'https://services.sentinel-hub.com/ogc/wms/f9cfc572-6834-40de-b93c-72e01194d954'
-    layer_params = {
-        'service': 'WMS',
-        'request': 'GetMap',
-        'layers': 'NDVI',
-        'format': 'image/png',
-        'transparent': True,
-        'width': 1000,
-        'height': 1000,
-        'bbox': f'{map1_bounds[0]},{map1_bounds[1]},{map1_bounds[2]},{map1_bounds[3]}' if map1_bounds else '',
-        'time': date.strftime('%Y-%m-%d')
-    }
-
-
-
-    # """ wms_layer1 = dl.WMSTileLayer(
-    #     url=url,
-    #     layers='NDVI-S2-L1C',
-    #     format='image/png',
-    #     transparent=True,
-    #     opacity=0.7,
-    #     attribution='NDVI'
-    # )
-
-    # wms_layer1.image = ndvi
-
-    # wms_layer2 = dl.WMSTileLayer(
-    #     url=url,
-    #     layers='NDVI-S2-L1C',
-    #     format='image/png',
-    #     transparent=True,
-    #     opacity=0.7,
-    #     attribution='NDVI'
-    # )
-
-    # wms_layer2.image = ndvi
-
-    # return wms_layer1, wms_layer2 """
 
 if __name__ == '__main__':
     app.run_server(debug=True)
