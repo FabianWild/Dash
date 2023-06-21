254d22eeeaec58927395bb5dc4e7f70c9d3cc4b4
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2023-06-21 03:05:37 +0200

    pfade angepasst

--- leaflet.py
+++ leaflet.py
@@ -1,20 +1,34 @@
 # import packages
-import dash
 from dash import Dash, html, dcc, Input, Output
 import dash_leaflet as dl
 import functions
-from datetime import date
+from datetime import date, timedelta
 
-app = dash.Dash(__name__)
+app = Dash(__name__)
 server = app.server
 
 innsbruck = (47.267222, 11.392778)
 
 # Open the GeoTIFF files
-band, bounds = functions.read_file(r'assets\data\01_13\2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_B01_(Raw).tiff')
+band, bounds = functions.read_file(r'assets\data\2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_B01_(Raw).tiff')
 
 # Define image bounds with extent
 image_bounds = [[bounds.bottom, bounds.left],[bounds.top, bounds.right]]
+
+# define disabled days
+start_date = date(2022, 1, 1)
+end_date = date(2022, 12, 31)
+delta = timedelta(days=1)
+able_days = [date(2022, 1, 13), date(2022, 2, 12), date(2022, 3, 14), date(2022, 4, 18), date(2022, 5, 11), date(2022, 6, 27), date(2022, 7, 17), date(2022, 8, 14), date(2022, 9, 23), date(2022, 10, 5), date(2022, 11, 27), date(2022, 12, 22),]
+disabled_days = []
+while start_date <= end_date:
+    if start_date in able_days:
+        start_date += delta
+        continue
+    # add current date to list
+    disabled_days.append(start_date)
+    # increment start date by timedelta
+    start_date += delta
 
 # Initialize the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -34,8 +48,10 @@ app.layout = html.Div(children=[
                 id='my-date-picker-single-1',  # Unique ID for the first DatePickerSingle
                 min_date_allowed=date(2022, 1, 1),
                 max_date_allowed=date(2022, 12, 31),
-                initial_visible_month=date(2022, 7, 17),
-                date=date(2022, 7, 17)
+                initial_visible_month=date(2022, 1, 13),
+                date=date(2022, 1, 13),
+                disabled_days=disabled_days,
+                style={'position': 'relative', 'zIndex': 9999}
             ),
             html.Div(style={'height': '2px'}),
             dl.Map([
@@ -44,22 +60,25 @@ app.layout = html.Div(children=[
                     [dl.Overlay(
                         dl.ImageOverlay(
                             id='timelayer1',
-                            url='assets/images/01_13/2022-01-13_True_color.jpg', bounds=image_bounds, opacity=1), name='RGB'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_False_color.jpg', bounds=image_bounds, opacity=1), name='False Color'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_Moisture_Index.jpg', bounds=image_bounds, opacity=1), name='Moisture Index'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_NDSI.jpg', bounds=image_bounds, opacity=1), name='NDSI'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_NDVI.jpg', bounds=image_bounds, opacity=1), name='NDVI'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_NDWI.jpg', bounds=image_bounds, opacity=1), name='NDWI')]
-                )
-            ], style={'width': '100%', 'height': '100vh', 'margin': "auto", "display": "block"}, center=innsbruck, zoom=12, id='map'),
+                            url='assets/images/2022-01-13_True_color.jpg', bounds=image_bounds, opacity=1), name='RGB'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_False_color.jpg', bounds=image_bounds, opacity=1), name='False Color'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_Moisture_Index.jpg', bounds=image_bounds, opacity=1), name='Moisture Index'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_NDSI.jpg', bounds=image_bounds, opacity=1), name='NDSI'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_NDVI.jpg', bounds=image_bounds, opacity=1), name='NDVI'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_NDWI.jpg', bounds=image_bounds, opacity=1), name='NDWI')]
+                ),
+                dl.LayerGroup(id="click1")
+            ], style={'width': '100%', 'height': '70vh', 'margin': "auto", "display": "block"}, center=innsbruck, zoom=12, id='map1'),
         ]),
         html.Div(className='six columns', children=[
             dcc.DatePickerSingle(
                 id='my-date-picker-single-2',  # Unique ID for the second DatePickerSingle
                 min_date_allowed=date(2022, 1, 1),
                 max_date_allowed=date(2022, 12, 31),
-                initial_visible_month=date(2022, 7, 17),
-                date=date(2022, 7, 17)
+                initial_visible_month=date(2022, 1, 13),
+                date=date(2022, 1, 13),
+                disabled_days=disabled_days,
+                style={'position': 'relative', 'zIndex': 9999}
             ),
             html.Div(style={'height': '2px'}),
             dl.Map([
@@ -68,14 +87,15 @@ app.layout = html.Div(children=[
                     [dl.Overlay(
                         dl.ImageOverlay(
                             id='timelayer2',
-                            url='assets/images/01_13/2022-01-13_True_color.jpg', bounds=image_bounds, opacity=1), name='RGB'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_False_color.jpg', bounds=image_bounds, opacity=1), name='False Color'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_Moisture_Index.jpg', bounds=image_bounds, opacity=1), name='Moisture Index'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_NDSI.jpg', bounds=image_bounds, opacity=1), name='NDSI'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_NDVI.jpg', bounds=image_bounds, opacity=1), name='NDVI'),
-                     dl.Overlay(dl.ImageOverlay(url='assets/images/01_13/2022-01-13-00_00_2022-01-13-23_59_Sentinel-2_L2A_NDWI.jpg', bounds=image_bounds, opacity=1), name='NDWI')]
-                )
-            ], style={'width': '100%', 'height': '100vh', 'margin': "auto", "display": "block"}, center=innsbruck, zoom=12, id='map2'),
+                            url='assets/images/2022-01-13_True_color.jpg', bounds=image_bounds, opacity=1), name='RGB'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_False_color.jpg', bounds=image_bounds, opacity=1), name='False Color'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_Moisture_Index.jpg', bounds=image_bounds, opacity=1), name='Moisture Index'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_NDSI.jpg', bounds=image_bounds, opacity=1), name='NDSI'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_NDVI.jpg', bounds=image_bounds, opacity=1), name='NDVI'),
+                     dl.Overlay(dl.ImageOverlay(url='assets/images/2022-01-13_NDWI.jpg', bounds=image_bounds, opacity=1), name='NDWI')]
+                ),
+                dl.LayerGroup(id="click2")
+            ], style={'width': '100%', 'height': '70vh', 'margin': "auto", "display": "block"}, center=innsbruck, zoom=12, id='map2'),
         ]),
     ]),
 ])
@@ -99,6 +119,18 @@ def update_map2(date_selected):
     # Generate the image URL based on the selected date
     image_url = f"assets/images/{date_selected}_True_color.jpg"
     return image_url
+
+# Define callback for first MapClick
+@app.callback(
+    Output('click1', 'children'),
+    [
+        Input('map1', 'click_lat_lng'),
+    ]
+)
+
+def map_click(click_lat_lng):
+    print(click_lat_lng)
+    return [dl.Marker(position=click_lat_lng, children=dl.Tooltip("({:.3f}, {:.3f})".format(*click_lat_lng)))]
 
 
 if __name__ == '__main__':
