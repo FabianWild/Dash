dda367e60fd5c92370891d4bc8997957b968ee44
FabianWild <Fabian.Wild1996@gmx.de>
2023-06-26 22:16:39 +0200

    finaler downscale

--- downscale.py
+++ downscale.py
@@ -1,25 +1,53 @@
 import os
+import glob
 import rasterio
 
-def downscale_tiff(directory):
-    for filename in os.listdir(directory):
-        if filename.endswith(".tiff"):
-            filepath = os.path.join(directory, filename)
-            with rasterio.open(filepath) as src:
-                profile = src.profile
-                profile.update(
-                    width=src.width // 2,
-                    height=src.height // 2
-                )
-                data = src.read(
-                    out_shape=(src.count, src.height // 2, src.width // 2),
-                    resampling=rasterio.enums.Resampling.cubic
-                )
-            with rasterio.open(filepath, 'w', **profile) as dst:
-                dst.write(data)
-            print(f"{filename} has been downscaled and replaced.")
+# Input folder containing TIFF files
+input_folder = 'assets/data'
 
-# Provide the directory path where the TIFF files are located
-directory_path = "assets/data"
+# Output folder to save the downscaled TIFF files
+output_folder = 'assets'
 
-downscale_tiff(directory_path)
+# Downsampling factor
+downscale_factor = 2  # Adjust the value as desired
+
+# Get a list of TIFF files in the input folder
+tiff_files = glob.glob(os.path.join(input_folder, '*.tiff'))
+
+for tiff_file in tiff_files:
+    # Open the input TIFF file
+    with rasterio.open(tiff_file) as src:
+        # Calculate the new dimensions
+        new_height = src.height // downscale_factor
+        new_width = src.width // downscale_factor
+
+        # Calculate the new transform
+        new_transform = rasterio.transform.Affine(
+            src.transform.a * downscale_factor,
+            src.transform.b,
+            src.transform.c,
+            src.transform.d,
+            src.transform.e * downscale_factor,
+            src.transform.f
+        )
+
+        # Create the output file path
+        output_file = os.path.join(output_folder, os.path.basename(tiff_file))
+
+        # Configure the output TIFF file
+        profile = src.profile
+        profile.update(
+            transform=new_transform,
+            height=new_height,
+            width=new_width
+        )
+
+        # Open the output TIFF file for writing
+        with rasterio.open(output_file, 'w', **profile) as dst:
+            # Perform the downsampling by reading and writing blocks
+            for ji, window in dst.block_windows(1):
+                src_data = src.read(window=window, masked=True)
+                dst_data = src_data[:, ::downscale_factor, ::downscale_factor]
+                dst.write(dst_data, window=window)
+
+        print(f"Downsampling complete for: {tiff_file}")
